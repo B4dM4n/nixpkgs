b658bcaa4199f0081386b6f265e769fd0fbd2a2c
Fabian Möller <fabianm88@gmail.com>
2019-09-10 17:27:11 +0200

    nomacs: add official plugins

--- pkgs/applications/graphics/nomacs/default.nix
+++ pkgs/applications/graphics/nomacs/default.nix
@@ -13,22 +13,38 @@
 , libraw
 , libtiff
 , quazip
+, enablePlugins ? true
 }:
 
+let
+  inherit (stdenv.lib) optional optionalString;
+in
 mkDerivation rec {
   pname = "nomacs";
   version = "3.12";
 
-  src = fetchFromGitHub {
+  src = [
+    (fetchFromGitHub {
+      owner = "nomacs";
+      repo = "nomacs";
+      name = "nomacs";
+      rev = version;
+      sha256 = "12582i5v85da7vwjxj8grj99hxg34ij5cn3b1578wspdfw1xfy1i";
+    })
+  ] ++ optional enablePlugins (fetchFromGitHub {
     owner = "nomacs";
-    repo = "nomacs";
-    rev = version;
-    sha256 = "12582i5v85da7vwjxj8grj99hxg34ij5cn3b1578wspdfw1xfy1i";
-  };
+    repo = "nomacs-plugins";
+    name = "nomacs-plugins";
+    rev = "3.12.0";
+    sha256 = "163dfm8w6wy4q2rax7rn4nvajgwvf7b94bchq5404i8ykcc35sdm";
+  });
 
   patches = [
     ./nomacs-iostream.patch
   ];
+  postPatch = optionalString enablePlugins ''
+    ln -s ../../nomacs-plugins plugins
+  '';
 
   enableParallelBuilding = true;
 
@@ -53,12 +69,13 @@ mkDerivation rec {
                 "-DENABLE_TIFF=ON"
                 "-DENABLE_QUAZIP=ON"
                 "-DENABLE_TRANSLATIONS=ON"
-                "-DUSE_SYSTEM_QUAZIP=ON"];
+                "-DUSE_SYSTEM_QUAZIP=ON"
+                "-DENABLE_PLUGINS=${if enablePlugins then "ON" else "OFF"}"];
 
   meta = with stdenv.lib; {
     homepage = https://nomacs.org;
     description = "Qt-based image viewer";
-    maintainers = [maintainers.ahmedtd];
+    maintainers = with maintainers; [ ahmedtd b4dm4n ];
     license = licenses.gpl3Plus;
     repositories.git = https://github.com/nomacs/nomacs.git;
     inherit (qtbase.meta) platforms;
