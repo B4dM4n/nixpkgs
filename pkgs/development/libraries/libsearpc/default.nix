5e5c404acec963f95f5c158640f70d6115957a28
Fabian Möller <fabianm88@gmail.com>
2020-04-29 18:37:01 +0200

    libsearpc: cleanup build

--- pkgs/development/libraries/libsearpc/default.nix
+++ pkgs/development/libraries/libsearpc/default.nix
@@ -1,6 +1,6 @@
-{stdenv, fetchFromGitHub, automake, autoconf, pkgconfig, libtool, python2Packages, glib, jansson}:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, libtool, python, glib, jansson }:
 
-stdenv.mkDerivation rec {
+(stdenv.mkDerivation rec {
   version = "3.2.0";
   pname = "libsearpc";
 
@@ -11,15 +11,13 @@ stdenv.mkDerivation rec {
     sha256 = "18i5zvrp6dv6vygxx5nc93mai2p2x786n5lnf5avrin6xiz2j6hd";
   };
 
+  outputs = [ "out" "lib" "dev" ];
+
   patches = [ ./libsearpc.pc.patch ];
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ automake autoconf libtool python2Packages.python python2Packages.simplejson ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig python ];
+  buildInputs = [ libtool python.pkgs.simplejson ];
   propagatedBuildInputs = [ glib jansson ];
-
-  postPatch = "patchShebangs autogen.sh";
-
-  preConfigure = "./autogen.sh";
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/haiwen/libsearpc";
@@ -28,4 +26,4 @@ stdenv.mkDerivation rec {
     platforms = platforms.linux;
     maintainers = with maintainers; [ ];
   };
-}
+})
