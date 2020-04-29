adcbded36670937506ba42fdc93e51103697185e
Fabian Möller <fabianm88@gmail.com>
2020-04-29 18:34:05 +0200

    ccnet: add toPythonModule

--- pkgs/tools/networking/ccnet/default.nix
+++ pkgs/tools/networking/ccnet/default.nix
@@ -11,29 +11,30 @@
 , openssl
 , sqlite
 }:
-stdenv.mkDerivation rec {
-  version = "6.1.8";
-  pname = "ccnet";
+python.pkgs.toPythonModule
+  (stdenv.mkDerivation rec {
+    version = "6.1.8";
+    pname = "ccnet";
 
-  src = fetchFromGitHub {
-    owner = "haiwen";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "1zhin6iccy251ciqsk82ly25hg10iahc8jf6z50nkfsxa016ccqn";
-  };
+    src = fetchFromGitHub {
+      owner = "haiwen";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "1zhin6iccy251ciqsk82ly25hg10iahc8jf6z50nkfsxa016ccqn";
+    };
 
-  outputs = [ "out" "lib" "dev" ];
+    outputs = [ "out" "lib" "dev" ];
 
-  nativeBuildInputs = [ pkgconfig which autoreconfHook vala python ];
-  buildInputs = [ libsearpc libuuid libevent sqlite openssl ];
+    nativeBuildInputs = [ pkgconfig which autoreconfHook vala python ];
+    buildInputs = [ libsearpc libuuid libevent sqlite openssl ];
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  meta = with stdenv.lib; {
-    homepage = "https://github.com/haiwen/ccnet";
-    description = "A framework for writing networked applications in C";
-    license = licenses.gpl3Plus;
-    platforms = platforms.linux;
-    maintainers = [ ];
-  };
-}
+    meta = with stdenv.lib; {
+      homepage = "https://github.com/haiwen/ccnet";
+      description = "A framework for writing networked applications in C";
+      license = licenses.gpl3Plus;
+      platforms = platforms.linux;
+      maintainers = [ ];
+    };
+  })
