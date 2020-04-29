2e74ae5b614a3ad78f96e93e8f4006c0b7c4735b
Fabian Möller <fabianm88@gmail.com>
2020-04-29 18:42:25 +0200

    seafile-shared: add toPythonModule

--- pkgs/misc/seafile-shared/default.nix
+++ pkgs/misc/seafile-shared/default.nix
@@ -1,48 +1,49 @@
 { stdenv, fetchFromGitHub, which, autoreconfHook, pkgconfig, vala, python, curl, libevent, libsearpc, sqlite, libuuid }:
-stdenv.mkDerivation rec {
-  pname = "seafile-shared";
-  version = "7.0.7";
+python.pkgs.toPythonModule
+  (stdenv.mkDerivation rec {
+    pname = "seafile-shared";
+    version = "7.0.7";
 
-  src = fetchFromGitHub {
-    owner = "haiwen";
-    repo = "seafile";
-    rev = "v${version}";
-    sha256 = "0vgzb923x2q2w1zgbc56d50a5qj9xm77lg7czfzg3va7vd921gy8";
-  };
+    src = fetchFromGitHub {
+      owner = "haiwen";
+      repo = "seafile";
+      rev = "v${version}";
+      sha256 = "0vgzb923x2q2w1zgbc56d50a5qj9xm77lg7czfzg3va7vd921gy8";
+    };
 
-  outputs = [ "out" "lib" "dev" ];
+    outputs = [ "out" "lib" "dev" ];
 
-  nativeBuildInputs = [
-    autoreconfHook
-    vala
-    pkgconfig
-    python
-    python.pkgs.wrapPython
-  ];
+    nativeBuildInputs = [
+      autoreconfHook
+      vala
+      pkgconfig
+      python
+      python.pkgs.wrapPython
+    ];
 
-  buildInputs = [
-    libuuid
-    sqlite
-    libsearpc
-    libevent
-    curl
-  ];
+    buildInputs = [
+      libuuid
+      sqlite
+      libsearpc
+      libevent
+      curl
+    ];
 
-  configureFlags = [ "--disable-console" ];
+    configureFlags = [ "--disable-console" ];
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  pythonPath = [ libsearpc ];
+    pythonPath = [ libsearpc ];
 
-  postFixup = ''
-    wrapPythonPrograms
-  '';
+    postFixup = ''
+      wrapPythonPrograms
+    '';
 
-  meta = with stdenv.lib; {
-    homepage = "https://github.com/haiwen/seafile";
-    description = "Shared components of Seafile: seafile-daemon, libseafile, libseafile python bindings, manuals, and icons";
-    license = licenses.gpl3;
-    platforms = platforms.linux;
-    maintainers = [ ];
-  };
-}
+    meta = with stdenv.lib; {
+      homepage = "https://github.com/haiwen/seafile";
+      description = "Shared components of Seafile: seafile-daemon, libseafile, libseafile python bindings, manuals, and icons";
+      license = licenses.gpl3;
+      platforms = platforms.linux;
+      maintainers = [ ];
+    };
+  })
