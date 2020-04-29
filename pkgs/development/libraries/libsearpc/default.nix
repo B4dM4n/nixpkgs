e4e8ae5fb2d2728a12686c4d34b619db00fe8c38
Fabian Möller <fabianm88@gmail.com>
2020-04-29 18:37:59 +0200

    libsearpc: add toPythonModule

--- pkgs/development/libraries/libsearpc/default.nix
+++ pkgs/development/libraries/libsearpc/default.nix
@@ -1,29 +1,30 @@
 { stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, libtool, python, glib, jansson }:
 
-(stdenv.mkDerivation rec {
-  version = "3.2.0";
-  pname = "libsearpc";
+python.pkgs.toPythonModule
+  (stdenv.mkDerivation rec {
+    version = "3.2.0";
+    pname = "libsearpc";
 
-  src = fetchFromGitHub {
-    owner = "haiwen";
-    repo = "libsearpc";
-    rev = "v${version}";
-    sha256 = "18i5zvrp6dv6vygxx5nc93mai2p2x786n5lnf5avrin6xiz2j6hd";
-  };
+    src = fetchFromGitHub {
+      owner = "haiwen";
+      repo = "libsearpc";
+      rev = "v${version}";
+      sha256 = "18i5zvrp6dv6vygxx5nc93mai2p2x786n5lnf5avrin6xiz2j6hd";
+    };
 
-  outputs = [ "out" "lib" "dev" ];
+    outputs = [ "out" "lib" "dev" ];
 
-  patches = [ ./libsearpc.pc.patch ];
+    patches = [ ./libsearpc.pc.patch ];
 
-  nativeBuildInputs = [ autoreconfHook pkgconfig python ];
-  buildInputs = [ libtool python.pkgs.simplejson ];
-  propagatedBuildInputs = [ glib jansson ];
+    nativeBuildInputs = [ autoreconfHook pkgconfig python ];
+    buildInputs = [ libtool python.pkgs.simplejson ];
+    propagatedBuildInputs = [ glib jansson ];
 
-  meta = with stdenv.lib; {
-    homepage = "https://github.com/haiwen/libsearpc";
-    description = "A simple and easy-to-use C language RPC framework (including both server side & client side) based on GObject System";
-    license = licenses.lgpl3;
-    platforms = platforms.linux;
-    maintainers = with maintainers; [ ];
-  };
-})
+    meta = with stdenv.lib; {
+      homepage = "https://github.com/haiwen/libsearpc";
+      description = "A simple and easy-to-use C language RPC framework (including both server side & client side) based on GObject System";
+      license = licenses.lgpl3;
+      platforms = platforms.linux;
+      maintainers = with maintainers; [ ];
+    };
+  })
